fc8a1a0715b4f3db00b29f3a056b84e777bb976e
ruimgf <ruimgf95@gmail.com>
2018-07-30 21:22:04 +0100

    fix run directory

--- run_directory.py
+++ run_directory.py
@@ -2,18 +2,15 @@ import argparse
 import logging
 import time
 import glob
-import ast
 import os
-import dill
 
-import common
+
+from tf_pose import common
 import cv2
 import numpy as np
-from estimator import TfPoseEstimator
-from networks import get_graph_path, model_wh
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
 
-from lifting.prob_model import Prob3dPose
-from lifting.draw import plot_pose
 
 logger = logging.getLogger('TfPoseEstimator')
 logger.setLevel(logging.DEBUG)
@@ -29,29 +26,29 @@ if __name__ == '__main__':
     parser.add_argument('--folder', type=str, default='./images/')
     parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
-    parser.add_argument('--scales', type=str, default='[None]', help='for multiple scales, eg. [1.0, (1.1, 0.05)]')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+
     args = parser.parse_args()
-    scales = ast.literal_eval(args.scales)
+
 
     w, h = model_wh(args.resolution)
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
 
     files_grabbed = glob.glob(os.path.join(args.folder, '*.jpg'))
-    all_humans = dict()
     for i, file in enumerate(files_grabbed):
         # estimate human poses from a single image !
         image = common.read_imgfile(file, None, None)
+        if image is None:
+            logger.error('Image can not be read, path=%s' % args.image)
+            sys.exit(-1)
         t = time.time()
-        humans = e.inference(image, scales=scales)
+        humans = e.inference(image,resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
         elapsed = time.time() - t
 
         logger.info('inference image #%d: %s in %.4f seconds.' % (i, file, elapsed))
 
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
         cv2.imshow('tf-pose-estimation result', image)
-        cv2.waitKey(5)
+        cv2.waitKey(0)
 
-        all_humans[file.replace(args.folder, '')] = humans
-
-    with open(os.path.join(args.folder, 'pose.dil'), 'wb') as f:
-        dill.dump(all_humans, f, protocol=dill.HIGHEST_PROTOCOL)
